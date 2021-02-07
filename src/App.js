c992c3d0035c6e6be3c00dc949ed4c0e2e2aac5b
safa <bjsafa30@gmail.com>
2021-02-07 19:26:38 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { Form,Button } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+
+ <Form className="formulaire">
+   <h1>Contact</h1>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="Name" placeholder="Enter name" />
+    <Form.Label>Lastname</Form.Label>
+    <Form.Control type="Lastname" placeholder="Enter lastname" />
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="Email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+    </div>
+  );
+}
+
+export default App;
